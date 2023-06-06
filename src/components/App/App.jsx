c567b17abd4eb5628fab07b7856ed2f0b473f836
Oleksii Leschenko <djlabuh@gmail.com>
2023-06-07 00:11:21 +0300

    Rewritten to hooks App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -6,73 +6,61 @@ import { Searchbar } from 'components/Searchbar';
 import { Loader } from 'components/Loader';
 import { ImageGallery } from 'components/ImageGallery';
 import { Button } from 'components/Button';
-
 import { getImages } from '../../services/getImages';
 
-export class App extends Component {
-  state = {
-    searchText: '',
-    data: [],
-    isLoading: false,
-    page: 1,
-    maxPage: null,
-  };
+export const App = () => {
+  const [searchText, setSearchText] = useState('');
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [maxPage, setMaxPage] = useState(null);
 
-  componentDidUpdate(_, prevState) {
-    if (
-      prevState.searchText !== this.state.searchText ||
-      prevState.page !== this.state.page
-    ) {
-      this.setState({ isLoading: true }, () => {
-        this.performSearch();
-      });
-    }
-  }
-
-  handleSearch = searchText => {
-    this.setState({ searchText, page: 1, data: [] });
-  };
-
-  loadMoreImages = async () => {
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
-
-  performSearch = async () => {
-    const { searchText, page } = this.state;
-
+  const performSearch = useCallback(async () => {
     try {
-      const data = await getImages(searchText, page);
+      const searchData = await getImages(searchText, page);
 
-      if (data.hits.length === 0) {
+      if (searchData.hits.length === 0) {
         toast.error('Sorry, there are no images matching your query!');
         return;
       }
 
-      this.setState(prevState => ({
-        data: [...prevState.data, ...data.hits],
-        maxPage: Math.ceil(data.totalHits / 12),
-      }));
+      setData(prevData => [...prevData, ...searchData.hits]);
+      setMaxPage(Math.ceil(searchData.totalHits / 12));
     } catch (error) {
       toast.error(`Error: ${error}`);
     } finally {
-      this.setState({ isLoading: false });
+      setIsLoading(false);
     }
+  }, [searchText, page]);
+
+  useEffect(() => {
+    if (searchText !== '' || page !== 1) {
+      setIsLoading(true);
+      performSearch();
+    }
+  }, [searchText, page, performSearch]);
+
+  const handleSearch = searchText => {
+    setSearchText(searchText);
+    setPage(1);
+    setData([]);
   };
 
-  render() {
-    const { data, isLoading, page, maxPage } = this.state;
-    const showLoadMoreButton = data && data.length > 0 && page < maxPage;
+  const loadMoreImages = () => {
+    setPage(prevPage => prevPage + 1);
+  };
 
-    return (
-      <>
-        <Searchbar handleSearch={this.handleSearch} />
-        <section>
-          {isLoading && <Loader />}
-          <ToastContainer autoClose={2000} />
-          {data.length > 0 && <ImageGallery data={data} />}
-          {showLoadMoreButton && <Button onClick={this.loadMoreImages} />}
-        </section>
-      </>
-    );
-  }
-}
+  const showLoadMoreButton = data.length > 0 && page < maxPage;
+
+  return (
+    <>
+      <Searchbar handleSearch={handleSearch} />
+      <section>
+        {isLoading && <Loader />}
+        <ToastContainer autoClose={2000} />
+        {data.length > 0 && <ImageGallery data={data} />}
+        {showLoadMoreButton && <Button onClick={loadMoreImages} />}
+      </section>
+    </>
+  );
+};
