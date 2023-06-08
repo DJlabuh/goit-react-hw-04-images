eae2d5f19d3af396ccf03edfa929200e3158c550
Oleksii Leshchenko <112614267+DJlabuh@users.noreply.github.com>
2023-06-08 20:30:19 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -15,33 +15,6 @@ export const App = () => {
   const [page, setPage] = useState(1);
   const [maxPage, setMaxPage] = useState(null);
 
-  // const performSearch = async () => {
-  //   try {
-  //     const searchData = await getImages(searchText, page);
-
-  //     if (searchData.hits.length === 0) {
-  //       toast.error('Sorry, there are no images matching your query!');
-  //       return;
-  //     }
-
-  //     setData(prevData => [...prevData, ...searchData.hits]);
-  //     setMaxPage(Math.ceil(searchData.totalHits / 12));
-  //   } catch (error) {
-  //     toast.error(`Error: ${error}`);
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   if (!searchText) {
-  //     return;
-  //   }
-
-  //   setIsLoading(true);
-  //   performSearch();
-  // }, [searchText, page]);
-
   useEffect(() => {
     if (!searchText) {
       return;
